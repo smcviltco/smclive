cd2ce16f4ece5f35d54ed28d76e6c0a65649b79b
smcviltco <75363152+smcviltco@users.noreply.github.com>
2022-09-26 11:47:46 +0500

    Add files via upload

--- expense_statement/reports/reports_data.py
+++ expense_statement/reports/reports_data.py
@@ -14,10 +14,18 @@ class ExpenseStatementReport(models.AbstractModel):
              ('date', '=', date)]).mapped('debit'))
         return tot
 
+    def get_account_total(self, account):
+        model = self.env.context.get('active_model')
+        docs = self.env[model].browse(self.env.context.get('active_id'))
+        tot = sum(self.env['account.move.line'].search(
+            [('account_id', '=', account.id), ('move_id.branch_id', '=', docs.branch_id.id),
+             ('date', '>=', docs.date_from), ('date', '<=', docs.date_to)]).mapped('debit'))
+        return tot
+
     @api.model
     def _get_report_values(self, docids, data=None):
         accounts = self.env['account.account'].search([('seq_no', '>', 0)], order='seq_no asc')
-        move_lines = self.env['account.move.line'].search([('move_id.branch_id', '=', data["form"]['branch_id'][0]), ('account_id', 'in', accounts.ids), ('date', '>=', data["form"]['date_from']), ('date', '<=', data["form"]['date_to'])], order='')
+        move_lines = self.env['account.move.line'].search([('move_id.branch_id', '=', data["form"]['branch_id'][0]), ('account_id', 'in', accounts.ids), ('date', '>=', data["form"]['date_from']), ('date', '<=', data["form"]['date_to'])])
         dates = move_lines.mapped('date')
         dates = list(dict.fromkeys(dates))
         return {
@@ -27,4 +35,5 @@ class ExpenseStatementReport(models.AbstractModel):
             'accounts': accounts,
             'dates': dates,
             'get_date_data': self.get_date_data,
+            'get_account_total': self.get_account_total,
         }
